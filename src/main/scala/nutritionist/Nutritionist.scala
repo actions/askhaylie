0db09f1eebbbc02efdf207b341a214d7f9c9b608
Sergey Zelvenskiy <sergey@thinair.com>
2017-01-16 01:24:20 -0800

    data prep

--- src/main/scala/nutritionist/Nutritionist.scala
+++ src/main/scala/nutritionist/Nutritionist.scala
@@ -8,6 +8,7 @@ import org.joda.time.{DateTime, DateTimeZone}
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import scala.io.Source
 
 /**
   * Created by sergeyzelvenskiy on 1/14/17.
@@ -17,11 +18,31 @@ object Nutritionist {
 
   val dayToPhase = Array(1,1,2,2,3,3,3)
 
-  def currentPhase()={
+  def currentPhase(): String={
     val day = DateTime.now(DateTimeZone.forOffsetHours(-8)).dayOfWeek().get()
     s"P${dayToPhase(day)}"
   }
 
+  implicit def stringToBool(s: String): Boolean=s match {
+    case "1" => true
+    case "0" => false
+    case _  => throw new IllegalArgumentException(s"Don't know how to conver string $s to boolean")
+  }
+
+  val foods : Map[String, (String, Boolean, Boolean, Boolean)]  = Source.fromInputStream(this.getClass.getResourceAsStream("food.csv")).getLines().map(_.split(",")).collect{
+    case Array(food, kind, p1, p2, p3) => food -> (kind, stringToBool(p1), stringToBool(p2), stringToBool(p3))
+  }.toMap
+
+  def lookupFood(food: String ): Option[(String, Boolean, Boolean, Boolean)]={
+    val words = food.split(" ")
+    if(words.length==1){
+      foods.get(words(0))
+    } else {
+      None
+    }
+  }
+
+  def 
 
 }
 
@@ -44,6 +65,12 @@ class Nutritionist extends SpeechletV2{
           case (params,("Food", slot)) => params.copy(_1=slot.getValue)
           case (params,("Phase", slot)) => params.copy(_2=slot.getValue)
         }
+        lookupFood(food) match {
+          case Some((kind, p1, p2,p3))=>
+            getSpeechletResponse("It looks like the food you are looking for is not on the list.", "", false)
+          case None => getSpeechletResponse("It looks like the food you are looking for is not on the list.", "", false)
+        }
+      case _ => getSpeechletResponse("It looks like the food you are looking for is not on the list.", "", false)
     }
 
   }
@@ -64,6 +91,16 @@ class Nutritionist extends SpeechletV2{
     val (categoris, i) = splitArr(s.split("\n").filter(_.length>2))
     categoris.zipWithIndex.flatMap{case(names, i)=> names.map{Food(_,t(i), phase)}}
   }
+
+  def phaseProcess(s: String)=s.split(",").map(_.replace("P", "").toInt-1).foldLeft(new Array[Int](3)){case(arr, index) =>
+      arr(index)=1
+      arr
+    }
+
+  val preprocessed: Seq[(String,String, String)]
+
+  preprocessed.map{x=> x.copy(_1=x._1.replace(","," "), _3 = phaseProcess(x._3).mkString(","))}.map{case(name, kind, phase)=> s"$name,$kind,$phase"}
+
 
 
   override def onLaunch(requestEnvelope: SpeechletRequestEnvelope[LaunchRequest]): SpeechletResponse = {
